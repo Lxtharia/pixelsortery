6e1abbfebf3bc701cc93c7b11206d600fa7a657d
Lxtharia <maud-lin@gmx.de>
2024-03-14 21:28:35 +0100

    Add arguments for in/out path

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #![allow(unused)]
 
+use std::{collections::VecDeque, env, process::exit};
 use image::RgbImage;
 use std::time::{Duration, Instant};
 use pixelsorter::sorter::{Sorter, SortingMethod};
 
 fn main() {
-    let path: &str = "/home/xlein/Pictures/Wallpaper/landscape renatus.z wallpaper.jpg";
-//    let path: &str = "/home/xlein/Pictures/Wallpaper/proxy.png";
+    let mut args: VecDeque<String> = env::args().collect();
+    // shift
+    args.pop_front();
+
+    let path =  match args.get(0) {
+        Some(s) => s,
+        None => {println!("[!] You need to specify the input and the output path"); exit(1);},
+    };
+
+    let output_path =  match args.get(1) {
+        Some(s) => s,
+        None => {println!("[!] You need to specify the output path"); exit(1);},
+    };
+    
+    // let path: &str = "/home/xlein/Pictures/Wallpaper/proxy.png";
     
     // OPEN IMAGE 
     let img: RgbImage = image::open(path).unwrap().into_rgb8();
@@ -15,9 +29,9 @@ fn main() {
     println!("Sorting all the pixels...");
     let start = Instant::now();
 
-//    sort_whole_image(&mut img, &SortingMethod::Saturation);
-//    sort_whole_image(&mut img, &SortingMethod::Brightness);
-//    sort_whole_image(&mut img, &SortingMethod::Hue);
+    // sort_whole_image(&mut img, &SortingMethod::Saturation);
+    // sort_whole_image(&mut img, &SortingMethod::Brightness);
+    // sort_whole_image(&mut img, &SortingMethod::Hue);
     let sorter = Sorter {method: SortingMethod::Saturation};
     let mut ps = pixelsorter::Pixelsorter::new(img, sorter);
     ps.sort();
@@ -30,10 +44,10 @@ fn main() {
     println!("Time took to sort: {:?}", duration);
 
     /* SAVING */
-    let serial_num = 6;
-    let filename_mut = format!("./renatus-b-debug-{}.png", serial_num);
+    // let serial_num = 6;
+    // let filename_mut = format!("./renatus-b-debug-{}.png", serial_num);
     // let filename_out = format!("./outtest-{}.png", serial_num);
-    let _ = ps.save(filename_mut);
+    let _ = ps.save(output_path);
     // sorted_img_hb.save(filename_out);
 }
 
