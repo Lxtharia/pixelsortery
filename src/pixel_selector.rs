c51169f40436d80744102da5b33b8c83c9588b9a
Lxtharia <maud-lin@gmx.de>
2024-03-24 01:37:09 +0100

    Add random selector algorithm

--- src/pixel_selector.rs
+++ src/pixel_selector.rs
@@ -1,4 +1,5 @@
 use image::{RgbImage, Rgb, Pixel};
+use rand::{thread_rng, Rng};
 use crate::Span;
 
 pub trait PixelSelector {
@@ -19,6 +20,17 @@ pub struct RandomSelector {
 
 impl PixelSelector for RandomSelector {
     fn spans(&self, pixels: &Vec<&mut Rgb<u8>>) -> Vec<Span> {
-        unimplemented!()
+        let spans: Vec<Span> = Vec::new();
+        let len = pixels.len() as i32;
+        let mut rng = thread_rng();
+        let mut i = 0;
+        while i < len {
+            let r = rng.gen_range(0..self.length);
+            let mut end = i+r;
+            if end > len { end = len; }
+            println!("Range from {} to {}", i, i+r);
+            i += r + 1;
+        }
+       unimplemented!()
     }
 }
