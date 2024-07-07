2e23535a9ebe6e68ccf060cbf86e8351013fa7ee
Lxtharia <maud-lin@gmx.de>
2024-07-07 02:21:00 +0200

    Try to implement some command line options

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![allow(unused)]
 
 use image::RgbImage;
-use pixelsorter::span_sorter::{SpanSorter, SortingCriteria};
+use pixelsorter::pixel_selector::{PixelSelectCriteria, ThresholdSelector};
+use pixelsorter::span_sorter::{SortingCriteria, SpanSorter};
+use std::fmt::Arguments;
 use std::time::{Duration, Instant};
 use std::{collections::VecDeque, env, process::exit};
 
@@ -10,14 +12,14 @@ fn main() {
     // shift
     args.pop_front();
 
-    let path = match args.get(0) {
+    let path = match args.pop_front() {
         Some(s) => s,
         None => {
             println!("[!] You need to specify the input and the output path");
             exit(1);
         }
     };
-    let output_path = match args.get(1) {
+    let output_path = match args.pop_front() {
         Some(s) => s,
         None => {
             println!("[!] You need to specify the output path");
@@ -27,12 +29,38 @@ fn main() {
 
     // OPEN IMAGE
     let img: RgbImage = image::open(path).unwrap().into_rgb8();
+    let mut ps = pixelsorter::Pixelsorter::new(img);
+
+    // I should just use some argument library tbh
+    while let Some(arg) = args.pop_front() {
+        match arg.as_str() {
+            "--hue" => ps.sorter.criteria = SortingCriteria::Hue,
+            "--thres" => {
+                let mut thres = ThresholdSelector {
+                    min: 100,
+                    max: 200,
+                    criteria: PixelSelectCriteria::Hue,
+                };
+                // parse the string after that: --thres hue:10:200
+                if let Some(arg2) = args.pop_front() {
+                    let mut thres_opts: Vec<&str> = arg2.split(":").collect();
+                    thres.max = thres_opts.pop().unwrap_or("150").parse().unwrap_or(150);
+                    thres.min = thres_opts.pop().unwrap_or("50").parse().unwrap_or(50);
+                    thres.criteria = match thres_opts.pop().unwrap_or("hue") {
+                        "hue" => PixelSelectCriteria::Hue,
+                        _ => PixelSelectCriteria::Hue,
+                    };
+                }
+                ps.selector = Box::new(thres);
+            }
+            _ => print!("Unrecognized argument: {}", arg),
+        }
+    }
 
     // SORTING
     println!("Sorting all the pixels...");
     let start = Instant::now();
 
-    let mut ps = pixelsorter::Pixelsorter::new(img);
     //    ps.sort();
     //    ps.sorter.criteria = SortingCriteria::Hue;
     //    ps.sort();
@@ -44,12 +72,11 @@ fn main() {
     // let serial_num = 6;
     // let filename_mut = format!("./renatus-b-debug-{}.png", serial_num);
     // let filename_out = format!("./outtest-{}.png", serial_num);
-    let _ = ps.save(output_path);
+    let _ = ps.save(&output_path);
     // sorted_img_hb.save(filename_out);
     //    ps.sort_all_pixels();
-    ps.save(output_path);
-    ps.save(format!("full-{}", output_path));
+    ps.save(&output_path);
+    ps.save(format!("full-{}", &output_path));
 
     println!("Time took to sort: {:?}", duration);
-
 }
