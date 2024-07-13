14cc3210ceb8195d06a6fe04a352f1859718624a
Lxtharia <maud-lin@gmx.de>
2024-07-14 01:23:18 +0200

    just a temp commit, i wanna try something

--- src/span_sorter/glitchsort.rs
+++ src/span_sorter/glitchsort.rs
@@ -1,11 +1,13 @@
+use std::mem;
+
 use super::SortingCriteria;
 use crate::color_helpers::*;
 use ::array_init::array_init;
 use image::{Rgb, RgbImage};
 
 #[derive(Debug)]
-struct PixelWrapper{
-    px:  Rgb<u8>,
+struct PixelWrapper<'a, 'b>{
+    px: &'a mut &'b mut Rgb<u8>,
     val: u16,
 }
 
@@ -13,11 +15,17 @@ fn glitch_swap(wrapper_vec: &mut Vec<PixelWrapper>, x: usize, y: usize){
     // "Just" swap the Wrapper Objects in the Array and then change the color of the objects pixels
     wrapper_vec.swap(x,y);
 
-    let temp_val = wrapper_vec[x].px;
-    wrapper_vec[x].px = wrapper_vec[y].px;
-    wrapper_vec[y].px = temp_val;
+    println!("Before: {:?} and {:?}", wrapper_vec[x], wrapper_vec[y]);
 
-    // println!(">After: {:?} and {:?}", wrapper_vec[x], wrapper_vec[y]);
+    let (left, right) = wrapper_vec.split_at_mut(y);
+    mem::swap(left[x].px, right[0].px);
+
+//    let temp_val = wrapper_vec[x].px;
+//    wrapper_vec[x].px = wrapper_vec[y].px;
+//    wrapper_vec[y].px = temp_val;
+
+    println!(" After: {:?} and {:?}", wrapper_vec[x], wrapper_vec[y]);
+
 
     // The glitch comes from the fact that
     // We switch wrap_x and wrap_y and then swap their colors
@@ -33,11 +41,16 @@ fn glitch_swap(wrapper_vec: &mut Vec<PixelWrapper>, x: usize, y: usize){
 
 pub fn glitchsort_mut(pixels: &mut [&mut Rgb<u8>], value_function: for<'a> fn(&'a Rgb<u8>) -> u16){
     let span_len = pixels.len();
-    let mut fake_pixels = Vec::new();
+    let mut wrappers = Vec::new();
     // Wrap each pixel into a wrapper with a calculated value (TODO: from SortingCriteria)
-    pixels.into_iter().for_each(|px| {
-        let val = value_function(px);
-        fake_pixels.push(PixelWrapper{px: **px, val});
+    // Rgb([val as u8,0,40])
+    for i in 0..pixels.len() {
+        if(i<100){ println!("{}: {:?}", i,  pixels[i]);}
+    }
+
+    pixels.into_iter().for_each(|p| {
+        let val = value_function(p);
+        wrappers.push(PixelWrapper{px: p, val});
     });
     let mut gap = span_len;
     let mut swapped = false;
@@ -46,16 +59,17 @@ pub fn glitchsort_mut(pixels: &mut [&mut Rgb<u8>], value_function: for<'a> fn(&'
         swapped = false;
         for i in 0..span_len {
             if (gap + i >= span_len){break;}
-            if ( fake_pixels[i+gap].val < fake_pixels[i].val ){
-                glitch_swap(&mut fake_pixels, i+gap, i);
+            if ( wrappers[i+gap].val < wrappers[i].val ){
+                glitch_swap(&mut wrappers, i, i+gap);
                 swapped = true;
             }
         }
     }
 
-    for i in 0..pixels.len() {
-        *pixels[i] = fake_pixels[i].px;
-    }
+    // for i in 0..pixels.len() {
+    //     if(i<100){ println!("{:?} <- {:?}", pixels[i], wrappers[i].px);}
+    //     *pixels[i] = wrappers[i].px;
+    // }
 
 }
 
