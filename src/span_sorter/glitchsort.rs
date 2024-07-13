0307df4af97e0b88aac5d2282cebc23e796dd0ac
Lxtharia <maud-lin@gmx.de>
2024-07-14 01:51:30 +0200

    VICTORY! Achieve glitchsort
    Took way longer than it should have. Need to compare results more

--- src/span_sorter/glitchsort.rs
+++ src/span_sorter/glitchsort.rs
@@ -6,52 +6,39 @@ use ::array_init::array_init;
 use image::{Rgb, RgbImage};
 
 #[derive(Debug)]
-struct PixelWrapper<'a, 'b>{
-    px: &'a mut &'b mut Rgb<u8>,
+struct PixelWrapper {
+    ind: usize,
     val: u16,
 }
 
-fn glitch_swap(wrapper_vec: &mut Vec<PixelWrapper>, x: usize, y: usize){
+fn glitch_swap(pixels: &mut [&mut Rgb<u8>], wrapper_vec: &mut Vec<PixelWrapper>, x: usize, y: usize){
     // "Just" swap the Wrapper Objects in the Array and then change the color of the objects pixels
+
+    let i = wrapper_vec[x].ind;
+    let j = wrapper_vec[y].ind;
+
     wrapper_vec.swap(x,y);
+    let tmp = pixels[i].clone() ;
+    *pixels[i] = pixels[j].clone();
+    *pixels[j] = tmp;
 
-    println!("Before: {:?} and {:?}", wrapper_vec[x], wrapper_vec[y]);
-
-    let (left, right) = wrapper_vec.split_at_mut(y);
-    mem::swap(left[x].px, right[0].px);
-
-//    let temp_val = wrapper_vec[x].px;
-//    wrapper_vec[x].px = wrapper_vec[y].px;
-//    wrapper_vec[y].px = temp_val;
-
-    println!(" After: {:?} and {:?}", wrapper_vec[x], wrapper_vec[y]);
-
+    // This does not work:
+    // pixels.swap(i, j);
 
     // The glitch comes from the fact that
     // We switch wrap_x and wrap_y and then swap their colors
-    // But the value stays the same, which we use to further sort
-
-	// x is pointing to the pixel/color cx, y to cy
-	// we now swap x and y, because we sort these and they have to pull the actual color struct themselves
-	// what we do here is. We swap the pixels cx and cy. This puts these in the right order. 
-	// But x is still pointing to the previous address where cy is now located.
-	// So not only is the hue inaccurate to the pixel, if x were to swap with z, x would swap cy cause thats the pixel it points to 
-	// I don't know how to write efficient comments, but this will help me understand when i forget again
+    // But they still point to their initial index
 }
 
 pub fn glitchsort_mut(pixels: &mut [&mut Rgb<u8>], value_function: for<'a> fn(&'a Rgb<u8>) -> u16){
     let span_len = pixels.len();
     let mut wrappers = Vec::new();
-    // Wrap each pixel into a wrapper with a calculated value (TODO: from SortingCriteria)
-    // Rgb([val as u8,0,40])
+
     for i in 0..pixels.len() {
-        if(i<100){ println!("{}: {:?}", i,  pixels[i]);}
+        let val = value_function(pixels[i]);
+        wrappers.push(PixelWrapper{ind: i, val});
     }
 
-    pixels.into_iter().for_each(|p| {
-        let val = value_function(p);
-        wrappers.push(PixelWrapper{px: p, val});
-    });
     let mut gap = span_len;
     let mut swapped = false;
     while ( (gap > 1) || swapped ) {
@@ -60,16 +47,11 @@ pub fn glitchsort_mut(pixels: &mut [&mut Rgb<u8>], value_function: for<'a> fn(&'
         for i in 0..span_len {
             if (gap + i >= span_len){break;}
             if ( wrappers[i+gap].val < wrappers[i].val ){
-                glitch_swap(&mut wrappers, i, i+gap);
+                glitch_swap(pixels, &mut wrappers, i+gap, i);
                 swapped = true;
             }
         }
     }
-
-    // for i in 0..pixels.len() {
-    //     if(i<100){ println!("{:?} <- {:?}", pixels[i], wrappers[i].px);}
-    //     *pixels[i] = wrappers[i].px;
-    // }
 
 }
 
