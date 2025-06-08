52596ff73b43bd05c3a5c6f9a00a2e93db3547f5
Lxtharia <maud-lin@gmx.de>
2025-06-09 00:25:32 +0200

    Run video script in parallel

--- sort_video.sh
+++ sort_video.sh
@@ -20,7 +20,6 @@ while true ; do
             outfile=$1
         ;;
         *)
-            echo "Adding $1"
             sorteroptions+=("$1")
         ;;
     esac
@@ -30,7 +29,7 @@ done
 
 echo Sorting options: ${sorteroptions[@]}
 # printf "%s\n" "${sorteroptions[@]}"
-echo "in: $infile | out: $outfile"
+echo "=== in: '$infile' | out: '$outfile'"
 
 if [[ -z $infile || -z $outfile ]] ; then
     echo "You need to specify a input and an output file"
@@ -46,14 +45,16 @@ tmp_framelist=$(mktemp --suffix -framelist.txt)
 ### The difficulty is to retain the duration information about a frame
 ### ffmpeg can read all input files from a text file that includes the frame information though
 
-# Split to frames
-ffmpeg -i "$infile" "$tmp1/frame_%06d.png"
-
 # Get frame information/duration and save it to a file
+echo "=== Getting frame information"
 ffprobe -v error -i "$infile" -of csv=p=0 -select_streams v -show_entries packet=pts_time > "$tmp_timestamps"
 
-# Format the file that ffmpeg can read
+# Split to frames
+echo "=== Splitting video into frames..."
+ffmpeg -i "$infile" "$tmp1/frame_%06d.png" || exit -1
+    # -progress pipe:1 -loglevel warning 
 
+# Format the file that ffmpeg can read
 awk '{
   frame = sprintf("'$tmp2'/frame_%06d.png", NR)
   if (NR > 1) {
@@ -66,18 +67,20 @@ awk '{
   printf "duration %.6f\n", duration
 }' "$tmp_timestamps" > "$tmp_framelist"
 
-# Apply sorting
+echo "=== Sorting frames..."
+# Call command once to exit on wrong flags
+pixelsortery "${sorteroptions[@]/#}" --fixed 1 -i "$tmp1/frame_000001.png" -o /dev/null || exit -1
 
+# Apply sorting to every frame
 for f in "$tmp1/"*.png; do
     framename=$(basename "$f")
     echo "Processing $f ===> $tmp2/$framename"
     cp "$f" "$tmp2/$framename"
-    pixelsortery "${sorteroptions[@]/#}" -i "$f" -o "$tmp2/$framename" || {
-        echo "Something went wrong"
-        exit -1
-    }
+    pixelsortery "${sorteroptions[@]/#}" -i "$f" -o "$tmp2/$framename" &
 done
+wait
 
+echo "=== Creating video..."
 # Stitch back together
 ffmpeg -f concat -safe 0 -i "$tmp_framelist" -i "$infile" -map 0:v -map 1:a -shortest "$outfile"
     # -c:v libx264 -pix_fmt yuv420p -c:a copy 
