f27b5f6e1b3b61e9866fa232b5cdc03372b432ee
Lxtharia <maud-lin@gmx.de>
2025-08-22 13:22:43 +0200

    Add ability to provide pixelsorter options to mosaik script

--- scripts/mosaik.sh
+++ scripts/mosaik.sh
@@ -6,8 +6,13 @@ SIZE=$3
 PIX_BIN=pixelsortery
 
 function usage() {
-    echo "Usage: $0 <in> <out> 10x10"
+    echo "Usage: $0 <IN> <OUT> <NxM> [<OPTIONS>]"
+    echo "e.g. $0 in.png out.png 10x10 --right --thres hue:10:40"
     echo
+}
+function err() {
+    echo $@
+    exit -1
 }
 
 if [[ -z $1 ]]; then
@@ -22,11 +27,9 @@ if [[ -z $1 || -z $2 || -z $3 ]]; then
     usage
     exit -1
 fi
-
-function err() {
-    echo $@
-    exit
-}
+# Collect remaining arguments as arguments for the pixelsorter
+shift;shift;shift
+PIX_ARGS=(${@---vertical})
 
 TMPFOLDER=$(mktemp -d)
 : ${TMPFOLDER=/tmp/tmp.mosaik}
@@ -34,14 +37,15 @@ TMPFOLDER=$(mktemp -d)
 tileamount=${SIZE%%x*}
 [[ -z $tileamount || ! ( $tileamount -gt 0 ) ]] && err "Wrong syntax: $SIZE"
 
-magick "$IN" -crop "$SIZE"@ +repage +adjoin "$TMPFOLDER/tmp_%05d.png"  &&\
-    (
+echo "Splitting image into tiles..."
+magick "$IN" -crop "$SIZE"@ +repage +adjoin "$TMPFOLDER/tmp_%05d.png" \
+    && (
         for f in "$TMPFOLDER"/*; do
-            echo -en "sorting $f\r"
-            $PIX_BIN "$f" --vertical "$f"
+            echo -en "\rsorting $f "
+            $PIX_BIN -i "$f" -o "$f" ${PIX_ARGS[@]} || err ""
         done
-    ) && echo -e '\nRejoining tiles...' &&\
-    magick montage -mode concatenate -tile ${tileamount}x "$TMPFOLDER"/* $OUT
+    ) && echo -e '\nRejoining tiles...'\
+    && magick montage -mode concatenate -tile ${tileamount}x "$TMPFOLDER"/* $OUT
 
 rm -r "$TMPFOLDER"
 
